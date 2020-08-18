5f9aa0a80c1054f81858604b4fd23583282d4741
Ioneliness <68114682+Ioneliness@users.noreply.github.com>
2020-08-18 17:35:37 -0300

    Update api.py

--- api.py
+++ api.py
@@ -35,7 +35,7 @@ def get_timeline():
                     create_image_end(text)
                     author_user = f'@{tweet.author.screen_name}'
                     text = author_user
-                    api.update_with_media('images/atual.png', text, in_reply_to_status_id=tweet_id)
+                    api.update_with_media('images/atual.png', in_reply_to_status_id=tweet_id)
                     store_last_seen_id(tweet_id)
         except Exception as e:
             print(e)
